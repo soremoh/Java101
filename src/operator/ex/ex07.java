43c15f1dbafdf90e5099712bce059f8003bee26b
seungchan <psc9507@naver.com>
2022-09-18 13:00:00 +0900

    hello

---
+++ src/operator/ex/ex07.java
@@ -0,0 +1,10 @@
+package operator.ex;
+
+public class ex07  {
+    public static void main(String[] args) {
+
+        int number = 1234;
+        int result = number - number % 100;
+        System.out.println(result);
+    }
+}
